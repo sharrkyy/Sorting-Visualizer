28c37c178396e5cfa6ac3f066559ca084d4f939b
sharrkyy <eroskc10@gmail.com>
2026-08-13 20:04:02 +0545

    Remove test.cpp from repo

--- test.cpp
+++
@@ -1,3 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <optional>
-int main() { sf::RenderWindow w(sf::VideoMode({800,600}), "Test Window"); while(w.isOpen()) { while(const std::optional e = w.pollEvent()) { if(e->is<sf::Event::Closed>()) w.close(); } w.clear(sf::Color::Blue); w.display(); } return 0; }
